f93f5343699fd785f48ccb794342ebe281146cdf
justBeHonest <ahmethilmiask@gmail.com>
2022-06-30 23:40:06 +0300

    chore

--- lib/src/era_mode.dart
+++ lib/src/era_mode.dart
@@ -2,7 +2,8 @@ enum EraMode { BUDDHIST_YEAR, CHRIST_YEAR }
 
 int calculateYearEra(EraMode? era, int christYear) {
   if (era == EraMode.BUDDHIST_YEAR) {
-    return christYear + 543;
+    ///return christYear + 543; I CHANGE THIS LINE TO BELOW LINE
+    return christYear;
   }
   return christYear;
 }
